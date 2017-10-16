ee3db8123a6de38c774296099754fd17355254cd
Filippo Guida <filippo.guida@icloud.com>
2017-10-17 01:03:14 +0200

    .gitignore

---
+++ src/videodirt/VideoPlane.java
@@ -0,0 +1,36 @@
+package videodirt;
+
+import processing.core.PConstants;
+import processing.core.PImage;
+
+import java.nio.IntBuffer;
+
+class VideoPlane extends PImage {
+
+    private boolean active;
+    private int lastWidth;
+    private int lastHeight;
+
+    void init(IntBuffer int_buffer, int width, int height) {
+        if (lastWidth != width || lastHeight != height) {
+            init(width, height, PConstants.ARGB);
+            lastWidth = width;
+            lastHeight = height;
+            pixels = new int[width * height];
+        }
+        loadPixels();
+        int_buffer.rewind();
+        int_buffer.get(pixels, 0, width * height);
+        updatePixels();
+
+        active = true;
+    }
+
+    void clear() {
+        active = false;
+    }
+
+    boolean isActive() {
+        return active;
+    }
+}

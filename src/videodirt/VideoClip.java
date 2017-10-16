077be95d674aa85d5754e385a368fe36069a51f9
Filippo Guida <filippo.guida@icloud.com>
2017-10-17 01:00:03 +0200

    Upload Version 1.1.0: - definition of VideoPlane to manage incoming frames (instead of Movie) - customized thread pool's concurrency rules in VideoDirt - hardware accelereated video decoding performed (gstreamer1.0) - now VideoPlayer is inheriting by PlayBin (gst1-java bind) - new class VideoBridgeSink provide a bridge between gstreamer and processing context - customizable display function (in processing applet) - customizable network setup

--- src/videodirt/VideoClip.java
+++ src/videodirt/VideoClip.java
@@ -1,106 +1,137 @@
 package videodirt;
 
+import org.freedesktop.gstreamer.elements.PlayBin;
+
 import java.lang.reflect.Field;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.TimeUnit;
 
-import processing.core.PApplet;
-import processing.video.Movie;
+class VideoClip {
 
-public class VideoClip extends Movie {
+    private PlayBin player;
+    private boolean initialized;
 
-	private	static	List<VideoClip>	running_clips =	new CopyOnWriteArrayList<VideoClip>();
+    private float cps;
+    private float begin;
+    private float end = 1;
+    private float sustain;
+    private float legato;
+    private float speed = 1;            //max 4x (??)
+    private String unit = "r";
+    private int cut;
+    private float size = 1;
+    private float xpos;
+    private float ypos;
+    private int zpos;
+    private int blendmode;
+    private float opacity = 1;
+    private int mirror;
 
-	private float	cps;
-	
-	private float	begin;
-	private float	end		= 1;
-	private float	sustain;
-	private float	legato	= 1;
-	private int		cut;
-	
-	private float	speed	= 1;
-	private char 	unit		= 'r';
-	
-	private float	xsize	= 1;
-	private float	ysize	= 1;
-	private float	xpos	;
-	private float	ypos	;
-	private float	zpos;
 
-	//private float	exposure	= 1;
-	private float 	opacity	= 1;	
+    VideoClip(PlayBin player, Object[] args) {
+        this.player = player;
 
-	
-	VideoClip(PApplet parent, Object[] args) {
-		super(parent, VideoLibrary.getFilename(args));
-		decodeOSC(args);
-	}
-		
-	private void decodeOSC(Object[] args) {  //decode OSC arguments and set relative parameters
-		for (int i=0; i < args.length; i++) {
-			try {
-				Field field = getClass().getDeclaredField(args[i].toString());
-			    field.set(this, args[i+1]);
-			}
-			catch (Exception e) { /*do nothing on unrecognized field*/ }
-		}
-	}	
-	
-	public void play() {
-		super.play();
-		
-		if (sustain <= 0) sustain = super.duration();
-		begin = begin*sustain*legato;
-		end = end*sustain*legato;
-		jump(begin);
-		
-		switch(unit) {
-			case 'r': speed(speed);
-			break;
-			case 'c': speed((speed/cps)/super.duration());
-			break;
-			case 's': speed(speed/super.duration());
-			break;
-		}
+        //decode OSC
+        for (int i = 0; i < args.length; i += 2) {
+            try {
+                Field field = getClass().getDeclaredField(args[i].toString());
+                field.set(this, args[i + 1]);
+            } catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException e) {/*do nothing*/}
+        }
 
-		running_clips.add(this);
-		if (cut != 0) {
-			running_clips.removeIf(clip_to_check -> {
-				if (clip_to_check != this && clip_to_check.cut == cut) clip_to_check.dispose();
-				return (clip_to_check != this && clip_to_check.cut == cut);
-			});
-		}
-	}
-	
-	public void stop() {
-		super.stop();
-		running_clips.remove(this);
-		this.dispose();
-	}
-	
-	public float duration() {
-		return end-begin;
-	}
-		
-	public void display() {
-		if (available) read();
-		parent.tint(255, opacity*255);
-		parent.noStroke();	   
-		parent.beginShape(QUADS);
-		parent.texture(this);
-		parent.vertex(xpos*parent.width,			ypos*parent.height, 			zpos, 0, 0);
-		parent.vertex((xpos+xsize)*parent.width,	ypos*parent.height, 			zpos, 1, 0);
-		parent.vertex((xpos+xsize)*parent.width,	(ypos+ysize)*parent.height,	zpos, 1, 1);
-		parent.vertex(xpos*parent.width, 		(ypos+ysize)*parent.height,	zpos, 0, 1);
-		parent.endShape();
-	}
-	
-	public static void displayAll() {
-		running_clips.forEach(video -> video.display());
-	}
-	
-	public static boolean isRunning(VideoClip clip) {
-		return running_clips.contains(clip);
-	}
+        initialized = true;
+    }
+
+    boolean isInitialized() {
+        return initialized;
+    }
+
+    float getDuration() {
+        return (sustain <= 0) ? ((end - begin) * player.queryDuration(TimeUnit.SECONDS)) + legato : sustain + legato;
+    }
+
+    float getStartPoint() {
+        return begin * player.queryDuration(TimeUnit.SECONDS);
+    }
+
+    float getEndPoint() {
+        return getStartPoint() + getDuration();
+    }
+
+    float getSpeed() {
+        switch (unit) {
+            case "r":
+                return speed;
+            case "c":
+                return ((end - begin) * player.queryDuration(TimeUnit.SECONDS)) / (speed / cps);
+            case "s":
+                return ((end - begin) * player.queryDuration(TimeUnit.SECONDS)) / speed;
+            default:
+                return speed;
+        }
+    }
+
+    /*
+    public void stop() {
+        super.stop();
+        playing = false;
+    }
+
+    public boolean isPlaying() {
+        return playing;
+    }
+
+    public boolean isMirror() {
+        return mirror != 0;
+    }
+
+    public float getOpacity() {
+        return opacity;
+    }
+
+    public float getXPos() {
+        return xpos;
+    }
+
+    public float getYpos() {
+        return ypos;
+    }
+
+    public float getFrameRate() {
+        return getSourceFrameRate();
+    }
+
+    public int getBlendmode() {
+        switch(blendmode) {
+            case 0: return PConstants.BLEND;
+            case 1: return PConstants.ADD;
+            case 2: return PConstants.SUBTRACT;
+            case 3: return PConstants.DARKEST;
+            case 4: return PConstants.LIGHTEST;
+            case 5: return PConstants.DIFFERENCE;
+            case 6: return PConstants.MULTIPLY;
+            case 7: return PConstants.SCREEN;
+            case 8: return PConstants.REPLACE;
+            case 9: return PConstants.EXCLUSION;
+            case 10:return PConstants.OVERLAY;
+            case 11:return PConstants.HARD_LIGHT;
+            case 12:return PConstants.SOFT_LIGHT;
+            case 13:return PConstants.DODGE;
+            case 14:return PConstants.BURN;
+        }
+        return 0;
+    }
+
+    public void display() {
+        //if (available) read();
+        //parent.textureMode(PConstants.NORMAL);
+        parent.tint(255, opacity*255);
+        parent.noStroke();
+        parent.beginShape(QUADS);
+        parent.texture(this);
+        parent.vertex(xpos*parent.width, ypos*parent.height, zpos, 0, 0);
+        parent.vertex((xpos+1)*parent.width, ypos*parent.height, zpos, 1, 0);
+        parent.vertex((xpos+1)*parent.width, (ypos+1)*parent.height, zpos, 1, 1);
+        parent.vertex(xpos*parent.width, (ypos+1)*parent.height,	zpos, 0, 1);
+        parent.endShape();
+    }*/
 }
